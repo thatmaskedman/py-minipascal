8b1b65c2f362439e3fdac45a46d82146ff799b0f
Jordan Ashurst <user.email>
2020-04-10 01:32:13 -0700

    PEP8 compliance

--- lexer.py
+++ lexer.py
@@ -39,8 +39,8 @@ class Lexer:
                             lexeme,
                             self.dfa.current_state,
                             li_num)
-                    if self.dfa.next_state_final(char):
-                        if self.dfa.current_state in {100, 101, 102}:
+                    if (self.dfa.next_state_final(char):
+                         self.dfa.current_state in {100, 101, 102}):
                             self.dfa.clear()
                             self.dfa.change_state(char)
                             self.dfa.make_string()
