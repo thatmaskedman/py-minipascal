ad3dbafccc1633b71767465c51e8974fe4ff694c
Jordan Ashurst <user.email>
2020-04-10 12:39:43 -0700

    Removing Error class

--- error.py
+++
@@ -1,7 +0,0 @@
-class Error:
-    errors = {
-        500: "Number was expected",
-        501: "Unexpected end of file",
-        502: "Unexpected end of line",
-        503: "Ilegal character"
-    }
