955307da51950cf4dc0c34481dd95b0aca396738
Jordan Ashurst <user.email>
2020-04-09 17:01:15 -0700

    Error message fix

--- error.py
+++ error.py
@@ -4,5 +4,5 @@ class Error:
         501: "Wrong assignment operator",
         502: "Unexpected end of file",
         503: "Unexpected end of line",
-        504: "Incomplete character"
+        504: "Ilegal character"
     }
