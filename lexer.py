95bd9a0fd9189b1efdcb2dcb47e907f22565afae
Jordan Ashurst <bxordan@gmail.com>
2020-05-02 01:41:48 -0700

    Refactoring

--- lexer.py
+++ lexer.py
@@ -1,9 +1,3 @@
-class LexicalComponent:
-    def __init__(self, token, token_id, li_num):
-        self.token = token
-        self.token_id = token_id
-        self.li_num = li_num
-
 class Lexer:
     
     def __init__(self, dfa, keywords, sourcefile):
